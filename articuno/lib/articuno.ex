2109ea8f358a365b322f6d5685f78285c4ce46d5
Marko Vukovic <mvkvc.git@gmail.com>
2023-04-27 01:22:08 -0400

    Cleanup

--- articuno/lib/articuno.ex
+++ articuno/lib/articuno.ex
@@ -1,18 +1,2 @@
 defmodule Articuno do
-  @moduledoc """
-  Documentation for `Articuno`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Articuno.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- articuno/test/articuno_test.exs
+++ articuno/test/articuno_test.exs
@@ -1,8 +1,4 @@
 defmodule ArticunoTest do
   use ExUnit.Case
   doctest Articuno
-
-  test "greets the world" do
-    assert Articuno.hello() == :world
-  end
 end
