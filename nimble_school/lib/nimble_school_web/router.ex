8fa241f24e5565bbae394b784f2f7a301f697d50
Marko Vukovic <mvkvc.git@gmail.com>
2023-04-27 01:19:16 -0400

    Create nimble_school example site

--- nimble_school/lib/nimble_school_web/router.ex
+++ nimble_school/lib/nimble_school_web/router.ex
@@ -2,22 +2,29 @@ defmodule NimbleSchoolWeb.Router do
   use NimbleSchoolWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, {NimbleSchoolWeb.LayoutView, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, {NimbleSchoolWeb.LayoutView, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", NimbleSchoolWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
+  end
+
+  scope "/", NimbleSchoolWeb do
+    pipe_through(:browser)
+
+    get("/blog", BlogController, :index)
+    get("/blog/:id", BlogController, :show)
   end
 
   # Other scopes may use custom stacks.
@@ -36,9 +43,9 @@ defmodule NimbleSchoolWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      live_dashboard "/dashboard", metrics: NimbleSchoolWeb.Telemetry
+      live_dashboard("/dashboard", metrics: NimbleSchoolWeb.Telemetry)
     end
   end
 
@@ -48,9 +55,9 @@ defmodule NimbleSchoolWeb.Router do
   # node running the Phoenix server.
   if Mix.env() == :dev do
     scope "/dev" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end
